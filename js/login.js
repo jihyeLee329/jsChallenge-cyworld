0169d71b0b2e29e16dbe45ae12f4eba24ef3b270
jihyelee <jihyelee.329@gmail.com>
2022-04-23 01:33:06 +0900

    Fix : 경로 수정

--- js/login.js
+++ js/login.js
@@ -2,6 +2,6 @@ const loginBtn = document.querySelector('#login-form');
 
 function onSubmit(e){
     e.preventDefault();
-    window.location="main.html"
+    window.location="./main.html"
 }
 loginBtn.addEventListener('submit',onSubmit);
