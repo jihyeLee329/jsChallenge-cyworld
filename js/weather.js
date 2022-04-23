72a8558466c1db3376161049eb166ffe6bd92151
jihyelee <jihyelee.329@gmail.com>
2022-04-23 23:55:47 +0900

    Text : weather text 변경

--- js/weather.js
+++ js/weather.js
@@ -6,11 +6,11 @@ function onGeoSuccess(position){
     const lon = position.coords.longitude;
     const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${APIKEY}&units=metric`;
     fetch(url).then(response => response.json())
-    .then((data) =>{ console.log(data)
+    .then((data) =>{ 
        const temp = data.main.temp;
        const location = data.name;
        const weather = data.weather[0].main;
-       weatherForm.innerHTML = `<span>현재 위치 ${location}</span><br/><span>기온:${temp} / 기후:${weather}</span>`;
+       weatherForm.innerHTML = `현재 위치 <span>${location}</span><br/>기온 <span>${temp}°C</span> / 기후 <span>${weather}</span>`;
     });
 }
 //위치 잡아오기 실패 시
