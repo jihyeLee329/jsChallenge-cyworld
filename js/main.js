58786d333d7cf11e4c2f535c4c3af018c2b247a1
jihyelee <jihyelee.329@gmail.com>
2022-04-24 01:07:22 +0900

    Fix : localStorage key 값 null 일 경우 index로 리다이렉트

--- js/main.js
+++ js/main.js
@@ -1,3 +1,7 @@
 const greeting = document.querySelector('.greeting');
 const getUser = localStorage.getItem('username');
 greeting.innerHTML = `안녕,<br/> ${getUser}! `;
+console.log(getUser)
+if(getUser == null){
+    window.location="./index.html"
+}
